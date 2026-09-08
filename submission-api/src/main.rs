fc32b474ea548aface0fa15a0a8bf7208b57cd57
grayfox-labs <grayson.doshier@gmail.com>
2026-09-08 16:15:36 -0700

    feat(submission-api): create submission route and add dockerfile

--- submission-api/src/main.rs
+++ submission-api/src/main.rs
@@ -1,11 +1,32 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-  "Hello, world!"
+use std::thread;
+use rocket::serde::{Serialize, json::Json};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct SubmissionResult {
+  status: String,
+  testsPassed: u32,
+  testsFailed: u32,
+  totalTests: u32,
+}
+
+#[post("/submit/<id>")]
+fn submit(id: &str) -> Json<SubmissionResult> {
+  // Perform some processing for the submission
+  thread::sleep(std::time::Duration::from_secs(2));
+
+  // Respond with the submission result
+  Json(SubmissionResult {
+    status: "pass".to_string(),
+    testsPassed: 15,
+    testsFailed: 0,
+    totalTests: 15
+  })
 }
 
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/", routes![index])
+  rocket::build().mount("/", routes![submit])
 }
